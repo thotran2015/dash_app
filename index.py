cca02933edd1938bb7cc5417ba1dda42730a0893
Tho Tran <thotran9@mit.edu>
2020-01-11 13:26:15 -0500

    editing app

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from app import app
+from app import app, server
 from layouts import layout1, layout2, layout3
 import callbacks
 
