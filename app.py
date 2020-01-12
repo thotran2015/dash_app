d5f7c23e43fbcf55bb4905dbff46d1f40d27a720
Tho Tran <thotran9@mit.edu>
2020-01-11 19:33:42 -0500

    adding database

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import os
+import psycopg2
 import dash
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets= external_stylesheets)
 server = app.server
 app.config.suppress_callback_exceptions = True
+
+
+
 
     
     
