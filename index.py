29bac3703f403780f6bdff85b7ae3496315aaf4f
Tho Tran <thotran9@mit.edu>
2020-01-26 17:11:54 -0500

    simplify index file

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets= external_stylesheets)
 server = app.server
-app.config.suppress_callback_exceptions = True
+#app.config.suppress_callback_exceptions = True
 
 
 
